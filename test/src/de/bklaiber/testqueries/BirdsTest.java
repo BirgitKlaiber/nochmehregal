2ce0b04e8c44ed17335c8eb3a751141be0bb7a39
Erik Itter <Erik Itter>
2016-04-28 00:18:30 +0200

    beginning to make BirdsTest a resonable test

--- test/src/de/bklaiber/testqueries/BirdsTest.java
+++ test/src/de/bklaiber/testqueries/BirdsTest.java
@@ -1,16 +1,21 @@
 package de.bklaiber.testqueries;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.File;
+import java.util.Collection;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import de.bklaiber.inference.Inference;
 import edu.cs.ai.log4KR.relational.probabilisticConditionalLogic.kbParser.log4KRReader.Log4KRReader;
+import edu.cs.ai.log4KR.relational.probabilisticConditionalLogic.syntax.RelationalConditional;
 
 public class BirdsTest {
 
 	private static Inference inference = null;
+	private static RelationalConditional query = null;
 
 	@BeforeClass
 	public static void setup() {
@@ -19,9 +24,37 @@ public class BirdsTest {
 		inference.setKnowledgebase(new Log4KRReader(), new File("test/res/Birds.rcl"));
 	}
 
+	/**
+	 * Tests if the query can be processed.
+	 */
 	@Test
 	public void queryTest() {
 
+		Collection<RelationalConditional> generalization = inference.queryConditional(query);
+
+	}
+
+	/**
+	 * Tests if the generalization works as expected.
+	 */
+	@Test
+	public void checkGeneralization() {
+		Collection<RelationalConditional> generalization = inference.queryConditional(query);
+
+	}
+
+	/**
+	 * Tests if the runtime is below expected threshold.
+	 */
+	@Test
+	public void checkRuntimeOfGenerlization() {
+
+		long before = System.currentTimeMillis();
+
+		Collection<RelationalConditional> generalization = inference.queryConditional(query);
+
+		long after = System.currentTimeMillis();
+		assertEquals(after - before < 60 * 1000, true);
 	}
 
 }
