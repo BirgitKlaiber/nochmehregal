23ad50e118b34ea1879a1d4e171bcbf182abb027
BirgitKlaiber <birgit.klaiber@gmx.net>
2017-10-01 19:07:33 +0200

    kleine Aenderungen

---
+++ src/de/bklaiber/queries/QueryColdSpez.java
@@ -0,0 +1,12 @@
+package de.bklaiber.queries;
+
+import de.bklaiber.inference.Query;
+
+public class QueryColdSpez {
+	public static void main(String[] args) {
+		Query queryColdSpez = new Query("ressources/fileQuerys/ColdSpez.rcl");
+		queryColdSpez.query();
+
+	}
+
+}
---
+++ src/de/bklaiber/queries/QueryColdTransSpez.java
@@ -0,0 +1,12 @@
+package de.bklaiber.queries;
+
+import de.bklaiber.inference.Query;
+
+public class QueryColdTransSpez {
+	public static void main(String[] args) {
+		Query queryColdTransSpez = new Query("ressources/fileQuerys/ColdTransSpez.rcl");
+		queryColdTransSpez.query();
+
+	}
+
+}
