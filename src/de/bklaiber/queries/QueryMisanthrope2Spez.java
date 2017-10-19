e8f5612fb922d329aef8236acdf808f7020629a0
BirgitKlaiber <birgit.klaiber@gmx.net>
2017-10-19 12:01:30 +0200

    Beispiele Misanthrope2Spez und Misanthrope3Spez hinzufeguegt

---
+++ src/de/bklaiber/queries/QueryMisanthrope2Spez.java
@@ -0,0 +1,12 @@
+package de.bklaiber.queries;
+
+import de.bklaiber.inference.Query;
+
+public class QueryMisanthrope2Spez {
+	public static void main(String[] args) {
+		Query queryMisanthrope2Spez = new Query("ressources/fileQuerys/Misanthrope2Spez.rcl");
+		queryMisanthrope2Spez.query();
+
+	}
+
+}
---
+++ src/de/bklaiber/queries/QueryMisanthrope3Spez.java
@@ -0,0 +1,12 @@
+package de.bklaiber.queries;
+
+import de.bklaiber.inference.Query;
+
+public class QueryMisanthrope3Spez {
+	public static void main(String[] args) {
+		Query queryMisanthrope3Spez = new Query("ressources/fileQuerys/Misanthrope3Spez.rcl");
+		queryMisanthrope3Spez.query();
+
+	}
+
+}
