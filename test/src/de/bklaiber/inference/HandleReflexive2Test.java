7a92e66386f8a8c1718c7008839cdfaf75e3f733
BirgitKlaiber <birgit.klaiber@gmx.net>
2018-06-14 08:44:26 +0200

    Tests HandleReflexive und HandleReflexive2 implementiert

---
+++ test/src/de/bklaiber/inference/HandleReflexive2Test.java
@@ -0,0 +1,58 @@
+package de.bklaiber.inference;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.Vector;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.bklaiber.Utils.QueryReader;
+import de.bklaiber.testqueries.AbstractQueryTest;
+import edu.cs.ai.log4KR.relational.probabilisticConditionalLogic.kbParser.log4KRReader.Log4KRReader;
+import edu.cs.ai.log4KR.relational.probabilisticConditionalLogic.syntax.RelationalConditional;
+
+public class HandleReflexive2Test extends AbstractQueryTest {
+
+	@Before
+	public void setup() {
+		super.setup();
+
+		inference.setKnowledgebase(new Log4KRReader(), new File("test/res/Friendship.rcl"));
+		queries = new Vector<RelationalConditional>(QueryReader.readQueries(new File("test/res/Friendship.rcl")));
+	}
+
+	/**
+	 * Tests if the query can be processed.
+	 */
+	@Test
+	public void queryTest() {
+
+		super.queryTest();
+	}
+
+	@Test
+	public void checkGeneralizationPositive() {
+		Vector<String> generalizations = new Vector<String>();
+
+		generalizations.addElement("(likes(U,V))[0.6193173107763108]<U!=V>");
+		generalizations.addElement("(likes(U,V))[0.0]<U=V>");
+
+		Collection<RelationalConditional> groundInstances = inference.ground(queries.elementAt(0));
+		Collection<Collection<RelationalConditional>> classifiedClasses = inference.classify(queries.elementAt(0),
+				groundInstances);
+		CanonicalMinimumGeneralization generalization = (CanonicalMinimumGeneralization) inference.getGeneralization();
+
+		Vector<RelationalConditional> generalizedClasses = new Vector<RelationalConditional>(
+				generalization.generalizePositive(queries.elementAt(0), classifiedClasses));
+
+		assertEquals(generalizations.elementAt(0), generalizedClasses.elementAt(0).toString());
+		assertEquals(generalizations.elementAt(1), generalizedClasses.elementAt(1).toString());
+
+		assertEquals(generalizations.size(), generalizedClasses.size());
+
+	}
+
+}
